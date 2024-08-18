ebcf8f4faaf9da0bf8fad049fea98a798efd4b81
ItzMK <raomamoon592@gmail.com>
2024-08-18 12:09:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+console.log("Helo")
+carddata=[
+    {
+        id:1,
+        title:'McCafe',
+        des:'Browse our menu for more information on topics such as our tasty coffee and espresso drinks.',
+        image:'./images/mccafe-dsk-en.jpg',
+    },
+    {
+        id:2,
+        title:'McDelivery',
+        des:'Deliver food at your door step',
+        image:'./images/mcdelivery-574x249-en (1).jpg',
+    },
+    {
+        id:3,
+        title:'McDelivery',
+        des:'McDonalds in your pocket ',  
+        image:'./images/mccafe-dsk-en.jpg',
+    },
+    {
+        id:4,
+        title:'Our Menu',
+        des:'Our exclusive menu is waiting for you.',
+        image:'./images/burgers.jpeg.png',
+    },
+    {
+        id:5,
+        title:'Trending Now',
+        des:'Find out whats new.',
+        image:'./images/item-5.png',
+    },
+    {
+        id:6,
+        title:'Careers',
+        des:'Be a part of our team.',
+        image:'./images/careers-info-card-1.png',
+    },
+]
