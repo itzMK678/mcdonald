1fc4186ed0d5e373ef640ab1132d946843e4f558
ItzMK <raomamoon592@gmail.com>
2024-08-20 12:40:40 +0500

    responsive

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
+const showmenu =()=>{
+  const both=document.getElementById('both-nav');
 
-console.log("Helo")
-carddata=[
-    {
-        id:1,
-        title:'McCafe',
-        des:'Browse our menu for more information on topics such as our tasty coffee and espresso drinks.',
-        image:'./images/mccafe-dsk-en.jpg',
-    },
-    {
-        id:2,
-        title:'McDelivery',
-        des:'Deliver food at your door step',
-        image:'./images/mcdelivery-574x249-en (1).jpg',
-    },
-    {
-        id:3,
-        title:'McDelivery',
-        des:'McDonalds in your pocket ',  
-        image:'./images/mccafe-dsk-en.jpg',
-    },
-    {
-        id:4,
-        title:'Our Menu',
-        des:'Our exclusive menu is waiting for you.',
-        image:'./images/burgers.jpeg.png',
-    },
-    {
-        id:5,
-        title:'Trending Now',
-        des:'Find out whats new.',
-        image:'./images/item-5.png',
-    },
-    {
-        id:6,
-        title:'Careers',
-        des:'Be a part of our team.',
-        image:'./images/careers-info-card-1.png',
-    },
-]
+  if (both.style.display=='none') {
+    both.style.display='flex';
+  } else   {
+    both.style.display='none';
+    
+  }
+}
+const removealert=()=>{
+    document.getElementById('alert-container').style.display='none';
+}
+setTimeout(()=>{
+    document.getElementById('alert-container').style.display='flex';
+},3000)
